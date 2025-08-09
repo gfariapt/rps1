08f4c73c9a27c6c228e33644551ecad655aa23f2
gfariapt <goncalofaria74@gmail.com>
2025-08-10 00:01:49 +0100

    Implementacao RPS com UI

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+let humanScore = 0;
+let computerScore = 0;
+let gameOver = false;
+
+const resultadosDiv = document.querySelector("#results");
+const placarDiv = document.querySelector("#placar");
+const buttons = document.querySelectorAll(".btn");
+const finalResultDiv = document.querySelector("#finalResult");
+
+placarDiv.textContent = `Human Score: ${humanScore} <=> Computer Score: ${computerScore}`;
+
 function getComputerChoice(){
     let k = Math.floor(Math.random() * 3)
 
@@ -12,53 +23,49 @@ function getComputerChoice(){
     }
 }
 
-function getHumanChoice(){
-    let choice = prompt("rock, paper, scissors?")
-    return choice;
-}
-
-let humanScore = 0;
-let computerScore = 0;
-
 function playRound(humanChoice, computerChoice){
-    humanChoice = humanChoice.toLowerCase();
     
     if (humanChoice === computerChoice){
         console.log("Empataste esta ronda");
+        resultadosDiv.textContent = "Empataste esta ronda";
         return "empate";
     }
     else if((humanChoice === "rock" && computerChoice === "scissors") ||
             (humanChoice === "scissors" && computerChoice === "paper") ||
             (humanChoice === "paper" && computerChoice === "rock")){
             console.log("Ganhaste esta ronda");
+            resultadosDiv.textContent = "Ganhaste esta ronda";
+            humanScore++;
             return "ganhaste";
     }
     else{
         console.log("Perdeste esta ronda");
+        resultadosDiv.textContent = "Perdeste esta ronda";
+        computerScore++;
         return "perdeste";
     }
 }
 
-for(let i = 0; i < 5; i++){
-    let humanChoice = getHumanChoice();
-    let computerChoice = getComputerChoice();
 
-    let winner = playRound(humanChoice, computerChoice);
+buttons.forEach((button) =>{
+    button.addEventListener("click", (event) => {
+        if (gameOver) return;
 
-    if (winner === "ganhaste"){
-        humanScore++;
-    }
-    else if (winner === "perdeste"){
-        computerScore++;
-    }
-}
+        const humanChoice = event.target.textContent.toLowerCase();
+        console.log(`You picked ${humanChoice}`);
 
-if (humanScore > computerScore){
-    console.log("Ganhaste!");
-}
-else if (humanScore < computerScore){
-    console.log("Perdeste!");
-}
-else {
-    console.log("Empate!")
-}
+        playRound(humanChoice, getComputerChoice());
+        placarDiv.textContent = `Human Score: ${humanScore} <=> Computer Score: ${computerScore}`;
+
+        if (humanScore === 5){
+            console.log("Ganhaste!");
+            finalResultDiv.textContent = "Ganhaste o Jogo!";
+            gameOver = true;
+        }
+        else if (computerScore === 5){
+            console.log("Perdeste!");
+            finalResultDiv.textContent = "Perdeste o Jogo!";
+            gameOver = true;
+        }   
+    }) 
+});
